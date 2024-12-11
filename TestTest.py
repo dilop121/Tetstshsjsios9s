f1e14f6fb262352c80ac3078ff87ac9e5f2e1181
Dil <126651350+dilop121@users.noreply.github.com>
2024-12-11 11:11:48 +0530

    Update TestTest.py

--- TestTest.py
+++ TestTest.py
@@ -1,13 +1,12 @@
 import glob
-from os.path import dirname, isfile
+from os.path import basename, dirname, isfile
 
 
 def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+    mod_paths = glob.glob(dirname(file) + "/*.py")
 
     all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        basename(f)[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
@@ -16,4 +15,4 @@ def __list_all_modules():
 
 
 ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+all = ALL_MODULES + ["ALL_MODULES"]
